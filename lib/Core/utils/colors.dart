2fb9d1ed12d287d33dea9aba851783a5d8323859
MalekYasser26 <114422351+MalekYasser26@users.noreply.github.com>
2025-02-03 23:46:26 +0200

    added color that dropped when merging

--- lib/Core/utils/colors.dart
+++ lib/Core/utils/colors.dart
@@ -6,5 +6,5 @@ const Color subTextColor = Color(0xff1678F2);
 const Color unselectedContainerColor = Color(0xff7EB8FF);
 const Color textFieldColor = Color(0xffF1F6FB);
 const Color myWhiteColor = Colors.white;
-
+const Color bottomColor = Color(0xff7EB8FF);
 
